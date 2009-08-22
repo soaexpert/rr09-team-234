878e8c6fd9f4a870ddd30fcb739af61bd0d80cdc
Caue Guerra <caue.guerra@gmail.com>
2009-08-21 23:15:09 -0300

    index action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,12 @@
 class EventsController < ApplicationController
   def index
-    
+    @next_events = Event.find :all, 
+                              :conditions => ["date >= ?", Time.now], 
+                              :order => "date ASC", 
+                              :limit => 7
+    @past_events = Event.find :all, 
+                              :conditions => ["date < ?", Time.now], 
+                              :order => "date DESC",
+                              :limit => 7
   end
 end
