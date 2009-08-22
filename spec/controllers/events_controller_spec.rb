c6892c00b5330b2da58b5f1e4fcce474d19fb138
Caue Guerra <caue.guerra@gmail.com>
2009-08-21 23:31:31 -0300

    controller spec

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,9 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe EventsController do
 
-  #Delete this example and add some real ones
-  it "should use EventsController" do
-    controller.should be_an_instance_of(EventsController)
+  before(:each) do
+    @event = mock_model(Event)
+    @date = Time.now
+    Time.stub!(:now).and_return(@date)
+  end
+
+  it "should show the next and past events when at home" do
+    Event.should_receive(:find).with(:all, {:limit=>7, :order=>"date ASC", :conditions=>["date >= ?", @date]}).and_return([@event])
+    Event.should_receive(:find).with(:all, {:limit=>7, :order=>"date DESC", :conditions=>["date < ?", @date]}).and_return([@event])
+    get 'index'
   end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,11 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Event do
   before(:each) do
     @valid_attributes = {
-      
+      :label => "",
+      :name => "",
+      :description => "",
+      :date => Time.now,
+      :hits => 0
     }
   end
 
-  it "should create a new instance given valid attributes" do
-    Event.create!(@valid_attributes)
-  end
+
 end
