c94e4c41298dfdf8346fbb5e773ff52e689b8b2b
Felipe Kenobi <scaphe@gmail.com>
2009-08-22 05:01:25 -0300

    formatic plugin

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,9 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "authlogic-oid", :lib => "authlogic_openid"
+  
+  config.gem "justinfrench-formtastic", 
+      :lib     => 'formtastic', :source  => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
