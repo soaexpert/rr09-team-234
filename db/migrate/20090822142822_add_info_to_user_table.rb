e940f38d4c73655ab8a9bc1e39b7390db0762e49
Caue Guerra <caue.guerra@gmail.com>
2009-08-22 18:24:50 -0300

    fixing login

--- db/migrate/20090822142822_add_info_to_user_table.rb
+++ db/migrate/20090822142822_add_info_to_user_table.rb
@@ -1,7 +1,7 @@
 class AddInfoToUserTable < ActiveRecord::Migration
   def self.up
-    add_column :users, :name, :string
-    add_column :users, :email, :string
+    add_column :users, :name, :string, :default => nil, :null => true
+    add_column :users, :email, :string, :default => nil, :null => true
     add_column :users, :avatar_file_name, :string
     add_column :users, :avatar_content_type, :string
     add_column :users, :avatar_file_size, :integer
