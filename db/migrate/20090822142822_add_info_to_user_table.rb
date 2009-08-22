9d2b9a166b46ebb5990fd5a4e670f4ad46db3023
Caue Guerra <caue.guerra@gmail.com>
2009-08-22 11:52:52 -0300

    show event

---
+++ db/migrate/20090822142822_add_info_to_user_table.rb
@@ -0,0 +1,21 @@
+class AddInfoToUserTable < ActiveRecord::Migration
+  def self.up
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+    add_column :users, :avatar_file_name, :string
+    add_column :users, :avatar_content_type, :string
+    add_column :users, :avatar_file_size, :integer
+    add_column :users, :avatar_updated_at, :datetime
+    add_column :users, :last_login_at, :datetime
+  end
+
+  def self.down
+    remove_column :users, :last_login_at
+    remove_column :users, :avatar_updated_at
+    remove_column :users, :avatar_file_size
+    remove_column :users, :avatar_content_type
+    remove_column :users, :avatar_file_name
+    remove_column :users, :email
+    remove_column :users, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090822134747) do
+ActiveRecord::Schema.define(:version => 20090822142822) do
 
   create_table "events", :force => true do |t|
     t.string   "label",             :limit => 20
@@ -53,7 +53,14 @@ ActiveRecord::Schema.define(:version => 20090822134747) do
     t.string   "openid_identifier"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "persistence_token", :null => false
+    t.string   "persistence_token",   :null => false
+    t.string   "name"
+    t.string   "email"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.datetime "last_login_at"
   end
 
 end
