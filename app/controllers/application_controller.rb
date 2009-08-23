85be7c2a592c24cbc369558736551c8c0292e651
Rodrigo Yoshima <rodrigoy@gmail.com>
2009-08-23 20:24:48 -0300

    event page finally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,23 @@ class ApplicationController < ActionController::Base
     def location
       session[:geo_location]
     end
+  
+  protected
+  
+    def parse_videos(label, start_index, max_results)
+      video_list = []
+      feed_contents = "http://gdata.youtube.com/feeds/api/videos/-/#{label}?start_index=#{start_index}&max-results=#{max_results}"
+      doc = Hpricot(open(feed_contents))
+      
+      doc.search("feed//entry").each do |raw_item|
+        url = raw_item.at('media:player')['url']
+        thumbnail = raw_item.at('media:thumbnail')['url']
+        video = {:url => url, :thumbnail => thumbnail}
+        video_list << video
+      end
+      
+      video_list.reverse[0..1]
+
+    end
+
 end
