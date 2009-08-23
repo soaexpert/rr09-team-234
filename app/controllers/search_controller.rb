55dc0b9886874f99a4785e9a270e3572c7c306fc
Felipe Kenobi <scaphe@gmail.com>
2009-08-23 19:29:39 -0300

    paginating the search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,14 @@
 class SearchController < ApplicationController
-  def index
+   cattr_reader :per_page
+   @@per_page = 5 
+  def index    
         unless params[:q].blank? 
           query = params[:q].strip
-        end 
-        page  = params[:page] || 1
+        end     
         filters = {}
-        @sphinx_options = {:query => query}
+        @sphinx_options = {:query => query,:page => params[:page] || 1, :per_page => 20}
         @search = Ultrasphinx::Search.new(@sphinx_options).run
+        @total_entries = @search.total_entries
         @results =  @search.results
   end
 
